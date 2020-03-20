2c88e6bc9026f1921bdc1877e6f7870d3c282124
IntlUser <dyuzhev@gmail.com>
2020-03-20 19:05:26 +0300

    xslt renamed

---
+++ xslt_src/eee.bat
@@ -0,0 +1,6 @@
+mklink ogc.policy.core.xsl ogc.standard.core.xsl
+mklink ogc.reference-model.core.xsl ogc.standard.core.xsl
+mklink ogc.user-guide.core.xsl ogc.standard.core.xsl
+mklink ogc.release-notes.core.xsl ogc.standard.core.xsl
+mklink ogc.engineering-report.core.xsl ogc.standard.core.xsl
+mklink ogc.community-standard.core.xsl ogc.standard.core.xsl
